d8863f470a61c6b518f1878f0015b5ede5fa65d8
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2021-09-29 15:06:09 +0300

    Docker ortamı hazırlandı

---
+++ app/public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+// try{
+//     $db = new PDO("mysql:host=host.docker.internal:3306;dbname=testdb", "root", "mypassword");
+// }catch(PDOException $e){
+//     print $e->getMessage();
+// }
+// $query = $db->query("SELECT * FROM persons", PDO::FETCH_ASSOC);
+// if ( $query->rowCount() ){
+//      foreach( $query as $row ){
+//           print $row['name']."<br>";
+//      }
+// }
