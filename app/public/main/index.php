40ce078b545afa87cff6543ad29b76410ad3cc9b
Mesut Yılmaz <mesut.yilmaz@stu.fsm.edu.tr>
2021-10-12 15:58:39 +0300

    Shared kaldırıldı

--- app/public/main/index.php
+++ app/public/main/index.php
@@ -19,7 +19,25 @@
 <? $dir = __DIR__;?>
 
 <!-- Navbar Begin  -->
-    <? include $dir.'/shared/navbar.php' ?>
+<nav class="navbar navbar-expand-lg sticky-top navbar-light" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/main/index">Haber Sitesi</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-left: 170px;" id="categories">
+      <!-- Kategoriler -->
+      </ul>
+      <form class="d-flex" id="buttons">
+      <!-- Buttonlar -->
+    </div>
+  </div>
+</nav>
+
+
+
+
 <!-- Navbar End  -->
 
 <div class="container">
